1d7e953af67cf253532b23a9fe336415f8b2c7f5
Gurmeet Sandhu <gurmeet990@gmail.com>
2019-09-17 14:14:13 -0700

    first Commit

---
+++ Questions/Question1/q1.js
@@ -0,0 +1,6 @@
+Difference between  Git and Github
+
+Git is a software or tool to create repository on our local machine and manage that 
+repository locally. Github is a version control system used host those repository on the 
+internet so anybody else can access it. we can push those repository to github by using tool
+git and create checkpoints.
